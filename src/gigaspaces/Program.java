c42470d7e0e7db7033da990bbf14bca26d704ca5
Lennart K <lennart.k@thinking-aloud.eu>
2015-04-09 09:55:18 +0200

    add basic source

---
+++ src/gigaspaces/Program.java
@@ -0,0 +1,35 @@
+package gigaspaces;
+
+import com.j_spaces.core.client.SQLQuery;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.GigaSpaceConfigurer;
+import org.openspaces.core.space.SpaceProxyConfigurer;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("Connecting to data grid");
+        SpaceProxyConfigurer configurer = new SpaceProxyConfigurer("myGrid");
+        configurer.lookupGroups("gigaspaces-10.1.0-XAPPremium-ga");
+        GigaSpace gigaSpace = new GigaSpaceConfigurer(configurer).create();
+
+        System.out.println("Write (store) a couple of entries in the data grid:");
+        gigaSpace.write(new Person(1, "Vincent", "Chase"));
+        gigaSpace.write(new Person(2, "Johnny", "Drama"));
+
+        System.out.println("Read (retrieve) an entry from the grid by its id:");
+        Person result1 = gigaSpace.readById(Person.class, 1);
+        System.out.println("Result: " + result1);
+
+        System.out.println("Read an entry from the grid using a SQL-like query:");
+        Person result2 = gigaSpace.read(new SQLQuery<Person>(Person.class, "firstName=?", "Johnny"));
+        System.out.println("Result: " + result2);
+
+        System.out.println("Read all entries of type Person from the grid:");
+        Person[] results = gigaSpace.readMultiple(new Person());
+        System.out.println("Result: " + java.util.Arrays.toString(results));
+
+        configurer.close();
+    }
+}
