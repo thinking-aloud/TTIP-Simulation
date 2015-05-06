a337169d90bd9eb4a9cef69e8383476f572012dc
Jo <joachimschole@me.com>
2015-05-06 15:57:21 +0200

    Car-Klasse aufgeteilt in Daten und Funktionalität

---
+++ src/gigaspaces/Car.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gigaspaces;
+
+import com.gigaspaces.annotation.pojo.SpaceId;
+
+/**
+ *
+ * @author jo
+ */
+public class Car {
+    
+    public enum DrivingDirection {
+        WEST,
+        EAST,
+        NORTH,
+        SOUTH
+    }
+    
+    private String id, positionRoxel;
+    private DrivingDirection drivingDirection;
+    
+    public Car(String pos, DrivingDirection dir) {
+        this.positionRoxel = pos;
+        this.drivingDirection = dir;
+    }
+    
+    public Car() {
+        
+    }
+
+    /**
+     * @return the id
+     */
+    @SpaceId(autoGenerate = true)
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the positionRoxel
+     */
+    public String getPositionRoxel() {
+        return positionRoxel;
+    }
+
+    /**
+     * @param positionRoxel the positionRoxel to set
+     */
+    public void setPositionRoxel(String positionRoxel) {
+        this.positionRoxel = positionRoxel;
+    }
+
+    /**
+     * @return the drivingDirection
+     */
+    public DrivingDirection getDrivingDirection() {
+        return drivingDirection;
+    }
+
+    /**
+     * @param drivingDirection the drivingDirection to set
+     */
+    public void setDrivingDirection(DrivingDirection drivingDirection) {
+        this.drivingDirection = drivingDirection;
+    }
+}
