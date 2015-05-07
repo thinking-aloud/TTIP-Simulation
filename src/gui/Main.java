dd4c15f2c68eaa448b41f65c13de9644a339b437
Lennart K <lennart.karsten@haw-hamburg.de>
2015-05-07 11:44:03 +0200

    fps really fixed now

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -16,10 +16,11 @@ import org.newdawn.slick.tiled.TiledMap;
 public class Main extends BasicGame {
 
     private TiledMap tiledMap;
-    static final int FPS = 1;
+    static final int FPS = 100;
     private final XapHelper xapHelper;
     private final ArrayList<CarContainer> carContainers;
     private final ArrayList<Thread> carContainerThreads;
+    private static AppGameContainer appgc;
 
     public Main(String gamename) {
         super(gamename);
@@ -36,13 +37,13 @@ public class Main extends BasicGame {
         xapHelper.initRoxels(mapWidth, mapHeight);
         xapHelper.initCars(mapWidth, mapHeight, FPS);
         Car cars[] = xapHelper.getCars();
-        
-        for (Car car: cars) {
+
+        for (Car car : cars) {
             CarContainer cc = new CarContainer(car);
             carContainers.add(cc);
             carContainerThreads.add(new Thread(cc));
         }
-        
+
         for (Thread t : carContainerThreads) {
             t.start();
         }
@@ -55,11 +56,11 @@ public class Main extends BasicGame {
     @Override
     public void render(GameContainer gc, Graphics g) throws SlickException {
         tiledMap.render(0, 0);
-        
+
         for (CarContainer container : carContainers) {
             container.getImage().draw(container.getX(), container.getY());
         }
-        
+
         // should be done like this, but the application is not thread save
 //        for (Car cars : xapHelper.getCars()) {
 //            CarContainer cc = new CarContainer(cars);
@@ -69,7 +70,6 @@ public class Main extends BasicGame {
 
     public static void main(String[] args) {
         try {
-            AppGameContainer appgc;
             appgc = new AppGameContainer(new Main("TTIP Simulation 2000"));
             appgc.setTargetFrameRate(FPS);
             appgc.setDisplayMode(1152, 768, false);
@@ -80,7 +80,13 @@ public class Main extends BasicGame {
     }
 
     public static int getFPS() {
-        return FPS;
+        int fps = appgc.getFPS();
+
+        if (fps > 0) {
+            return fps;
+        }
+
+        return 1;
     }
-    
+
 }
